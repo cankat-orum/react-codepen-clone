e4bf1f7aadd3ec8b11b92477da8ea086a1a25269
cankat-orum <jankatorumanp@gmail.com>
2023-11-09 16:19:45 +0300

    ADD open/close menu button
    - animations via framer-motion

--- src/container/Home.jsx
+++ src/container/Home.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {HiChevronDoubleLeft} from 'react-icons/hi2'
+import { motion } from 'framer-motion';
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+    const [isSideMenu, setIsSideMenu] = useState(false)
+  return <>
+    <div className={`w-2 ${isSideMenu ? "w-2" : "flex-[.2] xl:flex-[.2]"} min-h-screen max-h-screen relative bg-secondary px-3 py-6 flex flex-col items-center justify-start gap-4 transition-all duration-200 ease-in-out`}>
+        <motion.div whileTap={{scale: 0.9}} onClick={() => setIsSideMenu(!isSideMenu)} className='w-8 h-8 bg-secondary rounded-tr-lg rounded-br-lg absolute -right-6 flex items-center justify-center cursor-pointer'>
+            <HiChevronDoubleLeft className='text-white text-xl' />
+        </motion.div>
+        <div className='overflow-hidden w-full flex flex-col gap-4'>
+
+        </div>
+    </div>
+
+    <div></div>
+  </>
+};
 
 export default Home
