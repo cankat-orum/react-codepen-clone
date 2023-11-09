19ec5da9d546844fd9aab3e03b0d6fddf0f71289
cankat-orum <jankatorumanp@gmail.com>
2023-11-09 14:15:31 +0300

    CONFIGURE home route as homepage

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from "react";
+import { Home } from "./container";
+import { Navigate, Route, Routes } from "react-router-dom";
 
 const App = () => {
   return (
-    <div className="w-screen h-screen flex items-center justify-center">
-      App
+    <div className="w-screen h-screen flex items-start justify-start overflow-hidden">
+      <Routes>
+        <Route path="/home/*" element={<Home />} />
+        <Route path="*" element={<Navigate to={"/home"} />} />
+      </Routes>
     </div>
   );
 };
