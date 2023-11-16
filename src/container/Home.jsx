9c9c80948cba32774e2d9bc738f2d1625e218568
cankat-orum <jankatorumanp@gmail.com>
2023-11-16 14:46:23 +0300

    ADD right side top section ui
    - search logo
    - search box
    - signup button

--- src/container/Home.jsx
+++ src/container/Home.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import {HiChevronDoubleLeft} from 'react-icons/hi2'
 import {MdHome} from 'react-icons/md'
+import {FaSearchengin} from 'react-icons/fa6'
 import { motion } from 'framer-motion';
 import { Link } from 'react-router-dom';
 import { Logo } from '../assets'
@@ -31,7 +32,22 @@ const Home = () => {
         </div>
     </div>
 
-    <div></div>
+    <div className='flex-1 min-h-screen max-h-screen overflow-y-scroll h-full flex flex-col items-start justify-start px-4 md:px-12 py-4 md:py-12'>
+      <div className='w-full flex items-center justify-between gap-3'>
+        <div className='bg-secondary w-full px-4 py-3 rounded-md flex items-center justify-center gap-3'>
+          <FaSearchengin className='text-2xl text-primaryText' />
+          <input type="text" className='flex-1 px-4 py-1 text-xl bg-transparent outline-none border-none text-primaryText placeholder:text-gray-600' placeholder='Search here...' />
+        </div>
+        {!user && (
+          <motion.div whileTap={{scale: 0.9}} className='flex items-center justify-center gap-3'>
+            <Link to={"/home/auth"} className='bg-emerald-500 px-6 py-2 rounded-md text-white text-lg cursor-pointer hover:bg-emerald-700'>SignUp</Link>
+          </motion.div>
+        )}
+        {user && (
+          <div></div>
+        )}
+      </div>
+    </div>
   </>
 };
 
